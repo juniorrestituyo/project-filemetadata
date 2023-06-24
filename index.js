b91a530c3fa6bd2c0372de018cb47d9f1e56d236
Roberto Jr. Restituyo <101441219+juniorrestituyo@users.noreply.github.com>
2023-06-24 17:45:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
+
 require('dotenv').config()
 
-var app = express();
+const app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use(express.json({ extended: true }))
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
-});
+})
 
+const upload = multer({ dest: 'uploads/' }) .single('upfile')
 
+// Endpoint
+app.post('/api/fileanalyse', upload, (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
